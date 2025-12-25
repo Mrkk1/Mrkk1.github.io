59e3a44d68468c4bc73f591cd67fee674abeba80
yuweilong <60224145+Mrkk1@users.noreply.github.com>
2025-12-25 14:10:20 +0800

    fix: remove headers usage in utils for static export compatibility

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,17 +1,18 @@
-import { headers } from 'next/headers';
-import { UAParser } from 'ua-parser-js';
+// import { headers } from 'next/headers';
+// import { UAParser } from 'ua-parser-js';
 
 export const isMobileDevice = async () => {
-    if (typeof process === 'undefined') {
-        throw new Error('[Server method] you are importing a server-only module outside of server');
-    }
+    return false;
+    // if (typeof process === 'undefined') {
+    //     throw new Error('[Server method] you are importing a server-only module outside of server');
+    // }
 
-    const headersList = await headers();
-    const ua = headersList.get('user-agent');
+    // const headersList = await headers();
+    // const ua = headersList.get('user-agent');
 
-    const device = new UAParser(ua || '').getDevice();
+    // const device = new UAParser(ua || '').getDevice();
 
-    const isMobile = device.type === 'mobile';
+    // const isMobile = device.type === 'mobile';
 
-    return isMobile;
+    // return isMobile;
 };
